241bd505af3e13d284f8c5e8212e61e941c86241
musanna_nafis <hasanalmusanna7@gmail.com>
2022-02-01 20:23:12 +0600

    Lab Task 1

---
+++ Lab_Task1/resources/views/login.php
@@ -0,0 +1,14 @@
+<html>
+
+<form action="" method="">
+
+<input type="text" placeholder="Username"><br>
+
+<input type="password" placeholder="Password"><br>
+<input type="submit">
+
+
+
+</form>
+
+    </html>
